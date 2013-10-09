ffea19d337e3eb3a0c94c6b9863dbed98b354ac3
ffff <ffff@vm-0.ffff.koding.kd.io>
2013-10-09 03:35:10 +0000

    a simple py script for logging data to a csv

--- python-recording/test.py
+++ python-recording/test.py
@@ -1,50 +1,13 @@
-import mindwave, time
-from threading import Thread
-import datetime
-import Queue
 
 
-class MindWaveMonitor(Thread):
-    def __init__(self, out_queue):
-        Thread.__init__(self)
-        self.out_queue = out_queue
-        self.headset = mindwave.Headset('/dev/tty.MindWave', 'C61A')
-        self.headset.connect()
-        self._stop = False
-        time.sleep(2)
-
-    def run(self):
-        print "Attempting to connect with MindWave headset."
-        while self.headset.status != 'connected':
-            time.sleep(0.5)
-            if self.headset.status == 'standby':
-                self.headset.connect()
-        print "Connected."
-
-        while not self._stop:
-            time.sleep(1)
-            self.out_queue.put(EEGEvent(datetime.datetime.now(), self.headset.attention, self.headset.meditation))
-        
-        return
-
-class InteractionEvent:
-    def __init__(self, time):
-        self.time = time
-
-class EEGEvent(InteractionEvent):
-    def __init__(self, time, med, att):
-        InteractionEvent.__init__(self, time)
-        self.med = med
-        self.att = att
-
-
-event_queue = Queue.Queue()
-
+# set up a log 
+    
 headset = mindwave.Headset('/dev/tty.MindWave', 'C61A')
-def mindwaveHandler(set, data):
-    print "EEG (time, data): ", datetime.datetime.now(), " , ", data
+def mindwaveLogger(set, data):
+    with open('eeg-log.csv','a') as f:
+	f.write(datetime.datetime.now() + ', ' + data)
 
-headset.power_val_handlers.append(mindwaveHandler)
+headset.power_val_handlers.append(mindwaveLogger)
 
 
 time.sleep(2)
